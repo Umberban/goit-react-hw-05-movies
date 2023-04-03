3196e49df16ece2054d987c3b91a7cd6970ae527
Umberban <romanovborysv@gmail.com>
2023-04-04 01:45:54 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import {Navigation} from 'components/navigation/Navigation';
+import { Navigation } from 'components/navigation/Navigation';
 
-
+// dont work
 export const Layout = () => {
   return (
     <>
